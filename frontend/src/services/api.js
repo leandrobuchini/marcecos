f331643a9469aa9a8adc71e198557396b51076d7
Leandro Buchini <leandro_buchini@outlook.com>
2026-06-01 17:09:50 -0300

    sistema usuario produccion

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
-const api = axios.create({
+/*const api = axios.create({
   baseURL: 'http://localhost:4000/api'
+})*/
+
+const api = axios.create({
+  baseURL: 'https://marcecos-backend.onrender.com/api'
 })
 
 // Agrega el token automáticamente en cada petición
